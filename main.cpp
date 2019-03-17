752c0aa9f4c94c5286b28d1e312a2e22ab721e53
Alice (IvanovSE) <iapple1976@gmail.com>
2019-03-17 15:15:22 +0300

    code: частное чисел

--- main.cpp
+++ main.cpp
@@ -9,9 +9,6 @@ int main()
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
     << "A - B = " << a - b << '\n'
-<<<<<<< HEAD
+    << "A * B = " << a * b << '\n'
     << "A / B = " << a / b << '\n';
-=======
-    << "A * B = " << a * b << '\n';
->>>>>>> 6b9934ca09dcdc27599e12ba8ffbe9141c3b1f43
 }
